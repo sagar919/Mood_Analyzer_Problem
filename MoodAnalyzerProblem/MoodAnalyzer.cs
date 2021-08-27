f622ee9fd47949982638c3bd7f013bca361c1e6b
Sagar Lad <sagar.lad@morningstar.com>
2021-08-27 21:51:48 +0530

    Added Mood Analyzer Function

---
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    class MoodAnalyzer
+    {
+        public string AnalyzeMood(string message)
+        {
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))            
+                return "Sad Mood";
+            else
+                return "Happy Mood";
+
+        }
+
+        public static void Main(string[] args)
+        {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            //string mood = moodAnalyzer.AnalyzeMood("I am Sad");
+            Console.WriteLine("Enter your string");
+            string mood = Console.ReadLine();
+            Console.WriteLine(moodAnalyzer.AnalyzeMood(mood));
+        }
+        
+    }
+}
